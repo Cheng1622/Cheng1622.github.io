7ca9703f239e35db52e3f1012ec3c09c45a226f7
Cheng1622 <1622762650@qq.com>
2022-04-18 08:05:59 +0000

    Site updated: 2022-04-18 08:05:58

--- js/ip_content.js
+++ js/ip_content.js
@@ -47,19 +47,4 @@ $.get("https://ip.cjic.ga:444/", function (data, status) {
 });
 
 
-if ('serviceWorker' in navigator) {
-  // load 事件完成之后才注册 service worker
-  window.addEventListener('load', function () {
-      // 注册 sw.js
-      navigator.serviceWorker.register('/sw.js')
-          .then(function (registration) {
-              // 注册成功
-              console.log('sw.js 注册成功', registration.scope);
-          })
-          .catch(function (err) {
-              // 注册失败:(
-              console.log('sw.js 注册失败 ', err);
-          });
-  });
-}
 
